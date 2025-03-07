5d4ef112808d7bdfb40a2bf49c7959ca256e46a9
HardikMutha <hardikmutha2005@gmail.com>
2025-03-08 00:35:59 +0530

    Feat: Added Routing, Tailwind and removed Extras

---
+++ frontend/src/pages/OnBoarding.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const OnBoarding: React.FC = () => {
+  return (
+    <div>
+      <p className="text-3xl">
+        This is the onboarding page which will contain the three.js component
+      </p>
+    </div>
+  );
+};
+
+export default OnBoarding;
