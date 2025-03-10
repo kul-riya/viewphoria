074c3bfb5ded26b814bdee5981d2e19a8d6e82a8
HardikMutha <hardikmutha2005@gmail.com>
2025-03-10 21:54:28 +0530

    Add - Final API with data validation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
-from services.metadata_extractor.iceberg import get_metadata
+from api.routes.metadata import metadatarouter
 
 class ApiServer:
     def __init__(self):
         self.app = FastAPI()
 
+        self.app.include_router(metadatarouter, prefix="/metadata")
+        
         @self.app.get("/")
         async def read_root():
             return {"Hello World"}
         
-        @self.app.get('/metadata/iceberg/get_metadata')
-        async def read_metadata_iceberg():
-            buckets:str|None = await get_metadata(region_name='ap-south-1',aws_access_key_id='AKIAWCZC5Y5YUL6UBP34',aws_secret_access_key='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')
-            return buckets
-    
     def run(self, host="0.0.0.0", port=8000):
         uvicorn.run(self.app, host=host, port=port)
 
--- backend/app/services/metadata_extractor/iceberg.py
+++ backend/app/services/metadata_extractor/iceberg.py
@@ -1,10 +1,9 @@
 import boto3
 import json
-async def get_metadata(region_name:str,aws_access_key_id:str,aws_secret_access_key:str):
+async def get_metadata(region_name:str,aws_access_key_id:str,aws_secret_access_key:str,bucket_name:str):
     # s3 = boto3.resource(service_name='s3',region_name=region_name,aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
     # object_name = 'iceberg_warehouse/'
     s3_client = boto3.client('s3',region_name=region_name,aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
-    bucket_name = 'segfaultsurvivor'
     # This would list all the objects inside the iceberg i.e all the files which are present by scanning it recursively.
     # In the next step, I just extract all the files(objects) which end with .metadata.json as that is supposed to be unique
     #Then the metadata is parsed accordingly
@@ -17,10 +16,10 @@ async def get_metadata(region_name:str,aws_access_key_id:str,aws_secret_access_k
 
     try:
         response = s3_client.get_object(Bucket=bucket_name, Key=val)
-        metadata_content = response['Body'].read().decode('utf-8')  # Read & decode
+        metadata_content = response['Body'].read().decode('utf-8')
         metadata_json = json.loads(metadata_content)
-        print(metadata_json)
-        print(type(metadata_json),type(metadata_content))
+        # print(metadata_json)
+        # print(type(metadata_json),type(metadata_content))
         return metadata_json
     except Exception as e:
         print(str(e))
