4225716652e661d3e8a484d39052d4a9dc69fc36
amehta350 <amehta350@gmail.com>
2025-03-14 05:19:25 +0530

    SCRUM-14: initial metadata overview table added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import Home from "./pages/Home";
 import OnBoarding from "./pages/OnBoarding";
 import { Route, Routes } from "react-router";
+import MetadataOverviewTable from "./components/layout/MetadataOverviewTable";
 
 function App() {
   return (
+    <>
     <Routes>
       <Route path="/" element={<OnBoarding />} />
       <Route path="/home" element={<Home />} />
       {/* More routes can be added with the similar syntax, i.e just duplicate the Route tag and provide path and element  */}
     </Routes>
+
+    <MetadataOverviewTable/>
+    </>
   );
 }
 
