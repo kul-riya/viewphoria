db1da03ca1f5a6f0b5d4d78c2d04fecf2db7e909
xaman27x <aman27072005@gmail.com>
2025-03-26 19:14:16 +0530

    Auth route added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,19 @@ import { Route, Routes } from "react-router";
 import Temp from "./pages/Temp";
 import Protected from "./pages/Protected";
 import useAuthContext from "./hooks/useAuthContext";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
-  const { state } = useAuthContext();
   return (
     <>
       <Routes>
         <Route path="/" element={<OnBoarding />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
         {/* Temporary Routes to check out auth */}
         <Route path="/temp" element={<Temp />} />
         <Route
           path="/protected"
-          element={state.isAuthenticated ? <Protected /> : <Temp />}
         />
         {/* More routes can be added with the similar syntax, i.e just duplicate the Route tag and provide path and element  */}
       </Routes>
